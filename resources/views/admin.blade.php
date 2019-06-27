598f476fa876d81d8fbcf992779ccaf9ad9aa648
Yurii Adamenko <skyey07@gmail.com>
2019-06-27 21:54:06 +0300

    create auth user

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">USER Dashboard</div>
+                <div class="panel-heading">ADMIN Dashboard</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in as <strong>USER</strong>
+                    You are logged in as <strong>ADMIN</strong>
                 </div>
             </div>
         </div>
